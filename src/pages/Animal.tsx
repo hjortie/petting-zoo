e46978dcb4ee09d92cba4430bcdcaeb021df5783
hjortie <elinhjortskull@live.se>
2025-06-09 13:10:41 +0200

    Finding selected animal from context using the id from params.

--- src/pages/Animal.tsx
+++ src/pages/Animal.tsx
@@ -1,7 +1,26 @@
 import { useParams } from "react-router";
+import { AnimalContext } from "../contexts/AnimalContext";
+import { useContext } from "react";
 
 export const Animal = () => {
-  const params = useParams();
-
-  return <>{params.id}</>;
+  const { id } = useParams<{ id: string }>();
+  const { animals } = useContext(AnimalContext);
+  if (!id) {
+    console.error("No valid ID in URL");
+  }
+  const selectedAnimal = animals.find((a) => a.id === +id!);
+  if (!selectedAnimal) {
+    return <p>There is no animal matching this ID"</p>;
+  }
+  return (
+    <>
+      <div className="image-container">
+        <img src={selectedAnimal.imageUrl} alt={selectedAnimal.name} />
+      </div>
+      <h2>{selectedAnimal.name}</h2>
+      <p>{selectedAnimal.latinName}</p>
+      <p>{selectedAnimal.yearOfBirth}</p>
+      <span>{selectedAnimal.longDescription}</span>
+    </>
+  );
 };
