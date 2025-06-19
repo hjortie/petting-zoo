f631195d74bf5ad10549678647107aef0f60405a
hjortie <elinhjortskull@live.se>
2025-06-19 11:28:33 +0200

    set up for deploy

--- src/Router.tsx
+++ src/Router.tsx
@@ -3,13 +3,16 @@ import { Animal } from "./pages/Animal";
 import { Home } from "./pages/Home";
 import { Layout } from "./pages/Layout";
 
-export const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout />,
-    children: [
-      { path: "/", element: <Home /> },
-      { path: "/animal/:id", element: <Animal /> },
-    ],
-  },
-]);
+export const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        { path: "/", element: <Home /> },
+        { path: "/animal/:id", element: <Animal /> },
+      ],
+    },
+  ],
+  { basename: "/petting-zoo" }
+);
